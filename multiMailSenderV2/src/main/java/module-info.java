1cedb54b7bd6feb6ec7d5e3418075796f3d56e51
Murad014 <quliyev.murad@yahoo.com>
2023-10-18 19:20:44 +0400

    Delete All

--- multiMailSenderV2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.multimailsender.multimailsenderv2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.multimailsender.multimailsenderv2 to javafx.fxml;
-    exports com.multimailsender.multimailsenderv2;
-}
